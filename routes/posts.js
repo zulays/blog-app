4c9f11be7d67e0af915832ad460e439b5f3339fe
Zulay Scottborgh <zulayscottborgh@Zulays-MacBook-Pro.local>
2020-07-13 15:13:22 -0400

    routes updated

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,6 @@ router.get("/posts", controllers.getPosts)
 router.get("/posts/:id", controllers.getPost)
 router.post("/posts", controllers.createPost)
 router.put("/posts/:id", controllers.updatePost)
-router.delete("/posts/:id", controllers.deleteProduct)
+router.delete("/posts/:id", controllers.deletePost)
 
 module.exports = router
