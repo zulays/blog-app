84587bbacef1e54f2822d8e2e47c9baa9466a98c
Zulay Scottborgh <zulayscottborgh@Zulays-MacBook-Pro.local>
2020-07-13 16:32:02 -0400

    added space in connection file

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ let MONGODB_URI = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/postsAp
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
-    console.log("Successfully connected to MongoDB")
+    console.log("Successfully connected to MongoDB.")
   })
   .catch(e => {
     console.error("Connection error", e.message)
