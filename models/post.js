59d6e2b93f514ed1c6f14f704d741a733ff465b7
maliccny <maliccny@gmail.com>
2020-07-13 15:18:08 -0400

    fixed key names

--- models/post.js
+++ models/post.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 
 const Post = new Schema(
   {
-    name: { type: String, required: true },
+    title: { type: String, required: true },
     imgURL: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: String, required: true }
+    content: { type: String, required: true },
+    author: { type: String, required: true }
   },
   { timestamps: true }
 )
