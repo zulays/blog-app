010af4af4f51e2460f8ad4dcecb1accec2df2b06
Zulay Scottborgh <zulayscottborgh@Zulays-MacBook-Pro.local>
2020-07-14 11:13:25 -0400

    sort component udpated

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -1,23 +1,23 @@
-import React from "react"
-import "./Sort.css"
+import React from 'react'
+import './Sort.css'
 
 const Sort = (props) => {
 
-  const handleChange = (e) => {
-    props.onChange(e.target.value)
+  const handleChange = (event) => {
+    props.onChange(event.target.value)
   }
+
   return (
     <form className="sort-contain" onSubmit={props.handleSubmit}>
-      <label htmlFor="sort">Sort By:</label>
+      <label htmlFor="sort">Sort BY:</label>
       <select className="sort" onChange={handleChange}>
-        <option className="option" value="title-asc">&nbsp; Alphabetically, A-Z &nbsp;</option>
+        <option className="option" value="title-asc" >&nbsp; Alphabetically, A-Z &nbsp;</option>
         <option value="title-desc">&nbsp; Alphabetically, Z-A &nbsp;</option>
-        {/* <option className="option" value="name-asc">&nbsp; Alphabetically, A-Z &nbsp;</option>
-        <option className="option" value="name-asc">&nbsp; Alphabetically, A-Z &nbsp;</option> */}
+        {/* <option value="price-ascending">&nbsp; Price, low to high &nbsp;</option>
+        <option value="price-descending">&nbsp; Price, high to low &nbsp;</option> */}
       </select>
     </form>
   )
 }
-
 
 export default Sort
