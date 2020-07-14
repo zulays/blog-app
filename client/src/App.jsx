090e84a01c9057a18988a1e6a5118dfe0fa8c5cd
hi <kangja1996@gmail.com>
2020-07-14 15:11:26 -0400

    added App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./screens/Home/Home";
+import Posts from "./screens/Posts/Posts";
+import PostCreate from "./screens/PostCreate/PostCreate";
+import PostEdit from "./screens/PostEdit/PostEdit";
+import PostDetail from "./screens/PostDetail/PostDetail";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/add-post" component={PostCreate} />
+        <Route exact path="/posts/:id/edit" component={PostEdit} />
+        <Route exact path="/posts/:id" component={PostDetail} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
