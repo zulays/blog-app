078ca20b36fd2311e2832c9e723aae49eaac137f
Zulay Scottborgh <zulayscottborgh@Zulays-MacBook-Pro.local>
2020-07-14 11:52:33 -0400

    updated typo

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
         <NavLink className="logo" to="/">BlogApp</NavLink>
         <div className="links">
           <NavLink className="link" to="/posts">Posts</NavLink>
-          <NavLink className="link" to="/add-post">Add Product</NavLink>
+          <NavLink className="link" to="/add-post">Add Post</NavLink>
         </div>
       </div>
     </nav>
